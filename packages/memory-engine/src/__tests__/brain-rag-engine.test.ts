5f89c34c121290e1f41c7fc127ee0f9801d1e094
sierrablue8866-droid <sierrablue8866@gmail.com>
2026-09-20 09:29:52 +0300

    test: add unit tests for MemoryBrainEngine RAG functionality

--- packages/memory-engine/src/__tests__/brain-rag-engine.test.ts
+++ packages/memory-engine/src/__tests__/brain-rag-engine.test.ts
@@ -1,6 +1,6 @@
-import path from 'path';
-import fs from 'fs';
-import os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
 import { MemoryBrainEngine } from '../brain-rag-engine';
 
 describe('Unified Memory Brain Engine RAG (Obsidian + ECC)', () => {
